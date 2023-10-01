be08b5787c79759ca362241ce2e303e31607eeeb
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-10-01 12:05:56 +0300

    create restaurant pizza model

--- app/models.py
+++ app/models.py
@@ -14,3 +14,10 @@ class Pizza(db.Model):
     price = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant = db.relationship('Restaurant', backref=db.backref('restaurant_pizza'))
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizza'))
+
