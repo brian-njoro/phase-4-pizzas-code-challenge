920b6be5af18d904aa8b0c22ab344ca32afce858
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-10-01 11:53:33 +0300

    create Restaurant model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas')
