65c0bc2a575533030cd006f83c70cadfe177d7fa
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-10-01 11:56:26 +0300

    create Pizza model

--- app/models.py
+++ app/models.py
@@ -3,9 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'restaurants'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
-    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas')
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas', backref='restaurants')
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
